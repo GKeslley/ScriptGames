dbad5c7ab60f445e1355e0859116ec87287a5080
Guilherme Keslley <guilhermekeslley123@gmail.com>
2023-01-13 23:38:20 -0300

    Adicionando estilos do jogo

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 import airGame from "./js/airGame.js";
+import geniusGame from "./js/genius/genius.js";
 import memoryGame from "./js/memoryGame.js";
 import quizGame from "./js/quiz/quizGame.js";
 
 memoryGame();
 quizGame();
 airGame();
+geniusGame();
